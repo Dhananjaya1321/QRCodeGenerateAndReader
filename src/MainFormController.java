5c447df5022bd2616b5df1844aa7e3e7d07223ac
Dhananjaya1321 <blackisurudananjaya@gmail.com>
2023-02-20 01:06:11 -0800

    Add QR code generator and reader

---
+++ src/MainFormController.java
@@ -0,0 +1,15 @@
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+public class MainFormController {
+
+    @FXML
+    void QRCodeGeneratorOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void QRCodeReaderOnAction(ActionEvent event) {
+
+    }
+
+}
---
+++ src/QRCodeGenerator.java
@@ -0,0 +1,28 @@
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class QRCodeGenerator {
+    public static void generateQRcode(String data, String path, String charset, Map map, int h, int w) throws WriterException, IOException {
+        BitMatrix matrix = new MultiFormatWriter().encode(new String(data.getBytes(charset), charset), BarcodeFormat.QR_CODE, w, h);
+        MatrixToImageWriter.writeToFile(matrix, path.substring(path.lastIndexOf('.') + 1), new File(path));
+    }
+    public static void main(String args[]) throws WriterException, IOException {
+        String str= "https://github.com/Dhananjaya1321?tab=overview&from=2023-02-01&to=2023-02-19";
+        String path = "src\\Quoe.png";
+        String charset = "UTF-8";
+        Map<EncodeHintType, ErrorCorrectionLevel> hashMap = new HashMap<EncodeHintType, ErrorCorrectionLevel>();
+        hashMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+        generateQRcode(str, path, charset, hashMap, 200, 200);//increase or decrease height and width accodingly
+        System.out.println("QR Code created successfully.");
+    }
+}
